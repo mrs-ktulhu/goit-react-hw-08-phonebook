18ba9bac8da13ee1f39608fc49f35a9abd549a98
Mamba <113925095+mrs-ktulhu@users.noreply.github.com>
2023-11-27 18:57:07 +0200

    correct path

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import App from 'components/App';
-import { store, persistor } from './redux/store';
+import { store, persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
