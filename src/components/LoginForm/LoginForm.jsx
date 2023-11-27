03973c3b6b74058a8bffd76027b46b332eda7685
Mamba <113925095+mrs-ktulhu@users.noreply.github.com>
2023-11-27 18:51:05 +0200

    remove login fields validation

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,35 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { userLogin } from 'redux/AuthOperations';
-import Notiflix from 'notiflix';
+
 import { Link } from 'react-router-dom';
 import { LoginWrap, LoginBtn, LoginInput } from './LoginForm.styled';
 
 const LoginForm = () => {
   const dispatch = useDispatch();
-  const [hasError, setHasError] = useState({ email: false, password: false });
 
-  const handleChange = e => {
-    const { name } = e.target;
-    setHasError(prevErrors => ({ ...prevErrors, [name]: false }));
-  };
+
 
   const handleSubmit = e => {
     e.preventDefault();
     const form = e.currentTarget;
     const { email, password } = form.elements;
 
-    if (!email.value || !password.value) {
-      Notiflix.Notify.failure('Please, enter your information!', {
-        position: 'center-top',
-        distance: '10px',
-      });
-      setHasError({
-        email: !email.value,
-        password: !password.value,
-      });
-      return;
-    }
 
     dispatch(userLogin({ email: email.value, password: password.value }));
     form.reset();
@@ -42,7 +27,7 @@ const LoginForm = () => {
         <LoginInput>
           Email
           <br />
-          <input type="email" name="email" onChange={handleChange} required />
+          <input type="email" name="email"  required />
         </LoginInput>
         <br />
 
@@ -52,7 +37,7 @@ const LoginForm = () => {
           <input
             type="password"
             name="password"
-            onChange={handleChange}
+    
             required
           />
         </LoginInput>
